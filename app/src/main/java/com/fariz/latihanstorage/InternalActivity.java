508d3fe59240b8c6d4aa0cd8de01998a656a33c1
FarizPe <fariz.perdana212@gmail.com>
2021-10-22 22:55:36 +0700

    Aksi Baca dan Ubah belum ada

---
+++ app/src/main/java/com/fariz/latihanstorage/InternalActivity.java
@@ -0,0 +1,84 @@
+package com.fariz.latihanstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class InternalActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String FILENAME = "namefile.txt";
+    Button buatFile, ubahFile, bacaFile, deleteFile;
+    TextView textBaca;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internal);
+        buatFile = findViewById(R.id.buttonBuatFile);
+        ubahFile = findViewById(R.id.buttonUbahFile);
+        bacaFile = findViewById(R.id.buttonBacaFile);
+        deleteFile = findViewById(R.id.buttonHapusFile);
+        textBaca = findViewById(R.id.textBaca);
+
+        buatFile.setOnClickListener(this);
+        ubahFile.setOnClickListener(this);
+        bacaFile.setOnClickListener(this);
+        deleteFile.setOnClickListener(this);
+    }
+    void buatFile(){
+        String isiFile = "Coba Isi Data File Text";
+        File file = new File(getFilesDir(), FILENAME);
+
+        FileOutputStream outputStream = null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file, true);
+            outputStream.write(isiFile.getBytes());
+            outputStream.flush();
+            outputStream.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    void bacaFile(){
+
+    }
+    void ubahFile(){
+
+    }
+    void hapusFile(){
+        File file = new File(getFilesDir(), FILENAME);
+        if (file.exists()){
+            file.delete();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        jalankanperintah(v.getId());
+    }
+
+    public void jalankanperintah(int id) {
+        switch (id){
+            case R.id.buttonBuatFile:
+                buatFile();
+                break;
+            case R.id.buttonBacaFile:
+                bacaFile();
+                break;
+            case R.id.buttonUbahFile:
+                ubahFile();
+                break;
+            case R.id.buttonHapusFile:
+                hapusFile();
+                break;
+
+        }
+    }
+
+}
